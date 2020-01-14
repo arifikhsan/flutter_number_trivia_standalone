3c4b61b48cdc844678de2565398aba7167995e15
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-01-14 21:08:34 +0700

    network info

---
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_implementation.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_number_trivia_standalone/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'package:flutter_number_trivia_standalone/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
+import 'package:flutter_number_trivia_standalone/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+// class NumberTriviaRepositoryImplementation implements NumberTriviaRepository {
+  // final NumberTriviaRemoteDataSource remoteDataSource;
+  // final NumberTriviaLocalDataSource localDataSource;
+// }
