da6c3b0e48f6cd297c83d5aad0529005d68526bf
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-01-14 14:03:49 +0700

    initial commit

---
+++ lib/features/number_trivia/domain/entities/number_trivia_entity.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class NumberTriviaEntity {
+  final String text;
+  final int number;
+
+  NumberTriviaEntity({
+    @required this.text,
+    @required this.number,
+  });
+}
